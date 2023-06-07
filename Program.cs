2874ef726d92a2aaa86ad36292c3cdb15b23029e
Egbert Nierop <egbert.nierop@adccure.nl>
2023-06-07 22:31:37 +0200

    less is more

--- Program.cs
+++ Program.cs
@@ -1,12 +1,8 @@
 namespace Minyada;
 
-using System;
-using System.Security.Cryptography.X509Certificates;
 using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
-using System.Linq;
 using Microsoft.Extensions.Hosting;
 using SPA.Service.Models;
 using SPA.Service.Services;
@@ -32,46 +28,6 @@ public class Program
         services.AddControllers();
         ConfigureRedisCache(services, configuration);
 
-        var useUrls = configuration.GetValue("ASPNETCORE_URLS", $"{Uri.UriSchemeHttp}://0.0.0.0:80")!;
-        var pem = configuration.GetValue<string>("HTTPS_CERTIFICATE_PEM");
-        var pk = configuration.GetValue<string>("HTTPS_CERTIFICATE_PK");
-        IReadOnlyCollection<Uri> urisGiven = useUrls.Split(';').Select(s => new Uri(s)).ToArray();
-        if (File.Exists(pem))
-        {
-            using var certificate = X509Certificate2.CreateFromPemFile(pem, pk);
-            builder.WebHost.ConfigureKestrel(kestrelOptions =>
-            {
-                //find ssl
-                var splittedUrls = urisGiven.Where(s => s.Scheme == Uri.UriSchemeHttps) ?? throw new InvalidOperationException($"If certificate arn is given, a https uri must be configured {useUrls}");
-                if (certificate != null)
-                {
-                    foreach (var uri in splittedUrls)
-                    {
-                        kestrelOptions.ListenAnyIP(uri.Port, opt =>
-                        {
-                            opt.Protocols = HttpProtocols.Http1AndHttp2;
-                        });
-                    }
-                }
-
-                splittedUrls = urisGiven.Where(f => f.Scheme == Uri.UriSchemeHttp);
-
-                if (splittedUrls != null)
-                {
-                    foreach (var uri in splittedUrls)
-                    {
-                        kestrelOptions.ListenAnyIP(uri.Port);
-                    }
-
-                }
-            });
-
-        }
-        else
-        {
-            Console.WriteLine($"No PEM found");
-            builder.WebHost.UseUrls(urisGiven.Where(w => w.Scheme == Uri.UriSchemeHttp).Select(s => s.AbsoluteUri).ToArray());
-        }
         var app = builder.Build();
         // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
